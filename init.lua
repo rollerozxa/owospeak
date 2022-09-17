97db8c565aad85d0812fa713267c9bfdd164249a
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-09-17 17:36:11 +0200

    Implement being able to toggle OwOspeak on or off

--- init.lua
+++ init.lua
@@ -7,8 +7,33 @@ local owofaces = {
 	">w<"
 }
 
+local storage = minetest.get_mod_storage()
+local enabled = (storage:get('enabled')) or 1
+-- LOL
+enabled = (enabled == 1)
+
 minetest.register_on_chat_message(function(name, message)
-	minetest.chat_send_all(minetest.format_chat_message(name,
-		message:gsub(".", {r = "w", R = "W", l = "w", L = "W"}) .. " " .. owofaces[math.random(#owofaces)]))
-	return true
+	if enabled then
+		minetest.chat_send_all(minetest.format_chat_message(name,
+			message:gsub(".", {r = "w", R = "W", l = "w", L = "W"}) .. " " .. owofaces[math.random(#owofaces)]))
+
+		return true
+	end
 end)
+
+minetest.register_chatcommand("owospeak", {
+	params = "",
+	description = "Toggles OwOspeak",
+	privs = { server = true },
+	func = function(name, param)
+		if enabled then
+			minetest.chat_send_player(name, minetest.colorize("#ff0000", "Disabled OwOspeak"))
+			storage:set_int('enabled', 0)
+			enabled = false
+		else
+			minetest.chat_send_player(name, minetest.colorize("#00ff00", "Enabled OwOspeak"))
+			storage:set_int('enabled', 1)
+			enabled = true
+		end
+	end,
+})
